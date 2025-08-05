6b1fe40d9449664954b46324798d486874c9cf55
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2025-08-05 15:11:21 -0600

    useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 //import CounterApp from './01-useState/CounterApp'
-import CounterWithCustomHook from './01-useState/CounterWithCustomHook'
+//import CounterWithCustomHook from './01-useState/CounterWithCustomHook'
+import SimpleForm from './02-useEffect/SimpleForm'
 //import HooksApp from './HooksApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterWithCustomHook />
+    <SimpleForm />
   </React.StrictMode>,
 )
