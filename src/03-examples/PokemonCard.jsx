a03f4852218bcaa04faa67ea61b9afeb08c74d45
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2025-08-06 14:27:03 -0600

    Custom hook useFetch y guardar en cache

---
+++ src/03-examples/PokemonCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const PokemonCard = ({id, name, sprites = []}) => {
+    console.log(sprites);
+  return (
+    <div>
+        <section style={{height: 200}}>
+            <h2 className='text-capitalize'>#{ id } - { name } </h2>
+
+
+            <div>
+                {
+                    sprites.map( ( sprite ) => 
+                        <img key={ sprite } src={ sprite } alt={ name } />
+                     )
+                }
+            </div>
+
+        </section>
+    </div>
+  )
+}
+
+export default PokemonCard
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 //import CounterApp from './01-useState/CounterApp'
 //import CounterWithCustomHook from './01-useState/CounterWithCustomHook'
-import SimpleForm from './02-useEffect/SimpleForm'
+//import SimpleForm from './02-useEffect/SimpleForm'
+//import FormWithCustomHook from './02-useEffect/FormWithCustomHook'
+import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
 //import HooksApp from './HooksApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SimpleForm />
+      <MultipleCustomHooks />
   </React.StrictMode>,
 )
